df7ba1faab8696760053ebf0bbc4163cd6e4a1b4
Seonoh Eam <eso0609@naver.com>
2021-03-07 15:07:36 +0900

    restore test code

--- app/src/main/java/com/example/seonoh/mvvmsampleproject/BaseAcativity.kt
+++ app/src/main/java/com/example/seonoh/mvvmsampleproject/BaseAcativity.kt
@@ -12,4 +12,26 @@ abstract class BaseActivity<B : ViewDataBinding>(
     private val layoutRes: Int
 ) : AppCompatActivity() {
 
+    private lateinit var toast: Toast
+    protected lateinit var binding: B
+    abstract val viewModel: BaseViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.lifecycleOwner = this
+        toast = Toast.makeText(this, resources.getString("is back"), Toast.LENGTH_LONG)
+    }
+
+    private fun showToast() {
+        if (toast.view.isShown) {
+            finish()
+        } else {
+            toast.show()
+        }
+    }
+
+    override fun onBackPressed() {
+        showToast()
+    }
 }
