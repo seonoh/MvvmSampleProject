957f7df7cf4351f07af17005beb6aad39b11fea8
Seonoh Eam <eso0609@naver.com>
2021-03-14 14:17:46 +0900

    remove test file

--- app/src/main/java/com/example/seonoh/mvvmsampleproject/BaseViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.seonoh.mvvmsampleproject
-
-import androidx.lifecycle.ViewModel
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.disposables.Disposable
-
-abstract class BaseViewModel : ViewModel() {
-    
-}
